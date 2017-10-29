839302092d76b75e416856a4266efb2152d25ddf
ashmaltsuev <alex_shmaltsuev@intuit.com>
2017-10-29 11:08:36 +0200

    pic added

--- src/PlayerData.js
+++ src/PlayerData.js
@@ -3,9 +3,33 @@ import React, { Component } from 'react';
 class PlayerData extends Component {
 
     render() {
+        let typeOfData = this.props.entry.substring(this.props.entry.lastIndexOf(".") + 1,this.props.entry.length);
+        let item =this.getitem(typeOfData)
+
         return (
-            <li key={this.props.liKey}> {this.props.liKey}: <input type="text" id={this.props.liKey} onChange={this.props.handleInputChange} name={this.props.liKey} value={this.props.entry}/></li>
+        item
         );
+    }
+
+    getitem(typeOfData){
+        switch (typeOfData){
+            case 'png':
+                return<li key={this.props.liKey}> {this.props.liKey}:
+                        <input type="text" id={this.props.liKey} onChange={this.props.handleInputChange} name={this.props.liKey} value={this.props.entry}/>
+                        <div>
+                            <img src={this.props.entry}/>
+                        </div>
+                    </li>;
+                break;
+            case 'mp4':
+                return <li key={this.props.liKey}> {this.props.liKey}:
+                    <input type="text" id={this.props.liKey} onChange={this.props.handleInputChange} name={this.props.liKey} value={this.props.entry}/>
+
+                </li>;
+            default:
+                return <li key={this.props.liKey}> {this.props.liKey}: <input type="text" id={this.props.liKey} onChange={this.props.handleInputChange} name={this.props.liKey} value={this.props.entry}/></li>;
+
+        }
     }
 }
 
